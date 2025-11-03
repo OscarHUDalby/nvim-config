862804e2551ad2ec956edb736763f7096a8e40dc
OscarHUDalby <dalby.oscar@gmail.com>
2025-11-03 18:52:14 +0000

    changes to telescope

--- lua/plugins/telescope-file-browser.lua
+++ lua/plugins/telescope-file-browser.lua
@@ -2,7 +2,20 @@ return {
   "nvim-telescope/telescope-file-browser.nvim",
   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   config = function()
-    -- vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
+    require("telescope").setup({
+      defaults = {
+        file_ignore_patterns = { "node_modules/", "%.git/", "dist/", "%.cache/", "%.lock", "%.DS_Store" },
+      },
+      extensions = {
+        file_browser = {
+          grouped = false,          -- show files and folders in a flat list
+          respect_gitignore = true, -- obey .gitignore
+          hidden = true,            -- optionally show hidden files
+        },
+      },
+    })
+
+    require("telescope").load_extension "file_browser"
 
     -- open file_browser with the path of the current buffer
     vim.keymap.set("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
