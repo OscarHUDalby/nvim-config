fb28ab042e205694e0b7fd3bf0940546a518995e
OscarHUDalby <dalby.oscar@gmail.com>
2026-02-13 10:20:32 +0000

    disabled gh copilot for json and .env

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,10 @@
 return {
-	"github/copilot.vim",
+  "github/copilot.vim",
+  init = function()
+    vim.g.copilot_filetypes = {
+      json = false,
+      sh = false,     -- some .env files are shell scripts
+      dotenv = false, -- some .env files are detected as 'dotenv' filetype
+    }
+  end,
 }
